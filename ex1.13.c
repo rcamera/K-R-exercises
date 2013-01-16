4fc7badc9eeded1639755b84203069f5e87fa2fa
Rafael Câmera Santos <rafaelcamera@gmail.com>
2013-01-16 21:31:16 -0200

    Created a constant for maximum word length to show in the histogram in ex1.13.c

--- ex1.13.c
+++ ex1.13.c
@@ -1,47 +1,49 @@
 #include <stdio.h>
 
+#define MAX_LENGTH 10
+
 /*Print a horizontal histogram of the lengths of word from input*/
 /* a # equals 5% and a ] equals 2.5%*/
 
 main()
 {
 	int c, i, length;
-	double ndigit[10];
+	double ndigit[MAX_LENGTH];
 
 	length = 0;
-	for (i = 0; i < 10; ++i)
+	for (i = 0; i < MAX_LENGTH; ++i)
 		ndigit[i] = 0;
 
 	/*calculate length of each word, if length is over 10 chars, increment the last array value*/
 	while ((c = getchar()) != EOF)
 		if ((c == ' ' || c == '\n' || c == '\t') && length > 0){
-			if (length <=10)
+			if (length <= MAX_LENGTH)
 				++ndigit[length-1];
 			else
-				++ndigit[9];
+				++ndigit[MAX_LENGTH - 1];
 			length = 0;
 		}
 		else
 			++length;
 
 	/*if last character received before EOF wasn't either a \n, \t or ' ', then you need to count the last word as well*/
-	if(length > 0 && length <=10)
+	if(length > 0 && length <= MAX_LENGTH)
 		++ndigit[length-1];
-	else if (length > 10)
-		++ndigit[9];
+	else if (length > MAX_LENGTH)
+		++ndigit[MAX_LENGTH - 1];
 	length = 0;
 
 	/*Calculate total number of words*/
-	for (i = 0; i < 10; ++i)
+	for (i = 0; i < MAX_LENGTH; ++i)
 		length = ndigit[i] + length;
 
 	/*Calculate the percentage of each word length, given the total number of words*/
-	for (i = 0; i < 10; ++i)
+	for (i = 0; i < MAX_LENGTH; ++i)
 		ndigit[i] = ndigit[i] / length * 100;
 	
 	/*Print horizontal histogram with # representing 5% and ] representing 2.5%*/
-	for (i = 0; i < 10; ++i){
-		if (i == 9)
+	for (i = 0; i < MAX_LENGTH; ++i){
+		if (i == MAX_LENGTH - 1)
 			printf("%d+: \t", i+1);
 		else
 			printf("%d: \t", i+1);
